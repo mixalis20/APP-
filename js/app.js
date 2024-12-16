553ff3f2490a80289d8c0923bc31188026576e94
dimitris-saranteas <dimitrissar57@gmail.com>
2024-12-16 12:03:09 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -2,22 +2,32 @@ const selectImage = document.querySelector('.select-image');
 const inputFile = document.querySelector('#file');
 const imgArea = document.querySelector('.img-area');
 
+
+
 selectImage.addEventListener('click', function(){
     inputFile.click()
 })
 
-inputFile.addEventListener('click', function(event){
-    const image = event.target.files[0];
+function updateImage(event){
+    console.log("Updated")
+}
+
+inputFile.addEventListener("change",function(event){
+    console.log("updated",event.target.value)
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
     if (file && file instanceof Blob){
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const dataUrl = e.target.result; 
-            console.log(dataUrl);
-        };
-        reader.readAsDataURL(file);
-    } 
+   
+    reader.onload = function(e) {
+        const dataUrl = e.target.result; 
+        console.log(dataUrl);
+    };
+} 
     if (file instanceof Blob) {
-        reader.readAsDataURL(file);
-    } 
-    
-});
+    reader.readAsDataURL(file);
+} 
+})
+
+
+
