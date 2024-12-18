1941fc88dfb08064894a380b68659b8ad1e3d23e
dimitris-saranteas <dimitrissar57@gmail.com>
2024-12-18 16:32:23 +0200

    changes

--- js/login.js
+++ js/login.js
@@ -9,3 +9,40 @@ registerLink.addEventListener('click', ()=> {
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
+
+
+  
+// const validEmails = [
+//     "test@example.com",
+//     "user123@example.com",
+//     "admin@domain.com"
+// ];
+
+
+// function checkEmail() {
+//     const email = document.getElementById("emailInput").value;
+
+//     if (validEmails.includes(email)) {
+
+//         window.location.href = "/APP-/html/app.html"; 
+//     } else {
+//         alert("Email not recognized.");
+//     }
+// }
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();  
+
+    
+    const email = document.getElementById('email');
+    console.log();
+    // Validate email format using a regular expression
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (emailPattern.test(email)) {
+        
+        window.location.href = '/APP-/html/app.html';  
+    } else {
+        alert('Please enter a valid email address');
+    }
+});
